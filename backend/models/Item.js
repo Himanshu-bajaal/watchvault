c6b99192f09db6329bbc937c7baefdc5625378de
Himanshu-bajaal <himanshubajaal@gmail.com>
2026-09-10 14:14:35 +0530

    added schema using mongoose

---
+++ backend/models/Item.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+            trim: true,
+        },
+        type: {
+            type: String,
+            enum: ["movie" , "book"],
+            required: true,
+        },
+        status:{
+            type: String,
+            enum: ["want-to-watch" , "watched"],
+            default: "want-to-watch",
+        },
+        rating: {
+            type: Number,
+            min: 0,
+            max: 5,
+            default:null,
+        },
+        review: {
+            type: String,
+            default: '',
+        },
+    },
+    { timestamps: true,}
+);
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
