83dd06ad3f394abc560c42434dc33a5bcf928f3f
Himanshu-bajaal <himanshubajaal@gmail.com>
2026-09-14 18:12:26 +0530

    added login route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const User = require('../models/User');
 
 router.post('/signup', async (req, res) => {
@@ -26,6 +27,43 @@ router.post('/signup', async (req, res) => {
       user: { id: newUser._id, name: newUser.name, email: newUser.email },
     });
   } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    const token = jwt.sign(
+        {id: user._id},
+        process.env.JWT_SECRET,
+        {expiresIn: '7d'},
+    );
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      message: 'Login successful',
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  }
+  catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth.routes")
+const cookieParser = require("cookie-parser");
 
 
 dotenv.config();
@@ -9,9 +11,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true,
+}));
 app.use(express.json());
-
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
